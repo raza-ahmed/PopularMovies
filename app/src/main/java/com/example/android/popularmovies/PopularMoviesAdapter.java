fc88ac30cdf98fd1b03ac3921cae17fcd2e338a2
ahmedraza <raza2393@gmail.com>
2016-07-15 01:07:51 +0530

    Grid View implemented properly

--- app/src/main/java/com/example/android/popularmovies/PopularMoviesAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/PopularMoviesAdapter.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -63,7 +62,7 @@ public class PopularMoviesAdapter extends ArrayAdapter<PopularMovies> {
         TextView movieTitleView = (TextView) convertView.findViewById(R.id.list_item_movie_title);
         movieTitleView.setText(popularMovies.movieTitle);
 
-        EditText releasedateView = (EditText) convertView.findViewById(R.id.list_item_release_date);
+        TextView releasedateView = (TextView) convertView.findViewById(R.id.list_item_release_date);
         releasedateView.setText(popularMovies.releaseDate);
 
         return convertView;
