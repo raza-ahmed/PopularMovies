e4cf840033277bcd8582921115cb311352bcd4eb
ahmedraza <raza2393@gmail.com>
2016-07-14 18:46:38 +0530

    Cutom List View Adapter

--- app/src/main/java/com/example/android/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivityFragment.java
@@ -1,23 +1,35 @@
 package com.example.android.popularmovies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
-    private ArrayAdapter<String> movieDataAdapter;
+    private PopularMoviesAdapter moviesAdapter;
+
+    PopularMovies[] popularMovies = {
+
+            new PopularMovies("Movie Name1", "Release Date1", R.drawable.cupcake),
+            new PopularMovies("Movie Name2", "Release Date2", R.drawable.donut),
+            new PopularMovies("Movie Name3", "Release Date3", R.drawable.eclair),
+            new PopularMovies("Movie Name4", "Release Date4", R.drawable.froyo),
+            new PopularMovies("Movie Name5", "Release Date5", R.drawable.gingerbread),
+            new PopularMovies("Movie Name6", "Release Date6", R.drawable.honeycomb),
+            new PopularMovies("Movie Name7", "Release Date7", R.drawable.icecream),
+            new PopularMovies("Movie Name8", "Release Date8", R.drawable.jellybean),
+            new PopularMovies("Movie Name9", "Release Date1", R.drawable.kitkat),
+            new PopularMovies("Movie Name10", "Release Date1", R.drawable.lollipop),
+            new PopularMovies("Movie Name11", "Release Date1", R.drawable.eclair),
+    };
 
     public MainActivityFragment() {
     }
@@ -27,28 +39,12 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
 
-        String[] data={
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
 
-        };
+        moviesAdapter = new PopularMoviesAdapter(getActivity(), Arrays.asList(popularMovies));
 
-        List<String> movieData = new ArrayList<String>(Arrays.asList(data));
-
-        movieDataAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                R.layout.list_item_poular_movies,
-                R.id.list_item_release_date,
-                movieData
-        );
-
+        // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_popular_movies);
-        listView.setAdapter(movieDataAdapter);
+        listView.setAdapter(moviesAdapter);
 
         return rootView;
     }
---
+++ app/src/main/java/com/example/android/popularmovies/PopularMoviesAdapter.java
@@ -0,0 +1,72 @@
+package com.example.android.popularmovies;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by ahmedraza on 14/07/16.
+ */
+public class PopularMoviesAdapter extends ArrayAdapter<PopularMovies> {
+
+    private static final String LOG_TAG = PopularMoviesAdapter.class.getSimpleName();
+
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the List is the data we want
+     * to populate into the lists
+     *
+     * @param context        The current context. Used to inflate the layout file.
+     * @param popularMovies A List of PopularMovies objects to display in a list
+     */
+    public PopularMoviesAdapter(Activity context, List<PopularMovies> popularMovies){
+
+        super(context, 0, popularMovies);
+
+    }
+
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position    The AdapterView position that is requesting a view
+     * @param convertView The recycled view to populate.
+     *                    (search online for "android view recycling" to learn more)
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        // Gets the PopularMovies object from the ArrayAdapter at the appropriate position
+
+        PopularMovies popularMovies = getItem(position);
+
+        // Adapters recycle views to AdapterViews.
+        // If this is a new View object we're getting, then inflate the layout.
+        // If not, this view already has the layout inflated from a previous call to getView,
+        // and we modify the View widgets as usual.
+        if (convertView==null){
+            convertView= LayoutInflater.from(getContext()).inflate(R.layout.list_item_poular_movies,parent,false);
+        }
+        ImageView iconView = (ImageView) convertView.findViewById(R.id.list_item_image_banner);
+        iconView.setImageResource(popularMovies.image);
+
+        TextView movieTitleView = (TextView) convertView.findViewById(R.id.list_item_movie_title);
+        movieTitleView.setText(popularMovies.movieTitle);
+
+        TextView releasedateView = (TextView) convertView.findViewById(R.id.list_item_release_date);
+        releasedateView.setText(popularMovies.releaseDate);
+
+        return convertView;
+    }
+
+}
+
